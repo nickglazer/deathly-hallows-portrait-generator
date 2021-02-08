7f1ed5ea8f67ebc405b3fe1ac9ba3a26532d8e87
Nick Glazer <nglazerdev@gmail.com>
2021-02-08 18:36:01 -0500

    feat(skewedGrid): initial version of skewed grid generator

---
+++ skewedGrid/sketch.js
@@ -0,0 +1,112 @@
+let pixels = [], destinations = [];
+
+function setup() {
+  createCanvas(520, 520);
+  background(0);
+  stroke(255);
+
+  const form = document.getElementById('parameterForm');
+  form.addEventListener('submit', drawSketchHandler);
+
+  const randomForm = document.getElementById('randomForm');
+  randomForm.addEventListener('submit', drawRandomSketch);
+
+  const saveForm = document.getElementById('saveForm');
+  saveForm.addEventListener('submit', saveCanvasHandler);
+
+  drawRandomSketch();
+}
+
+const drawSketchHandler = e => {
+  e.preventDefault();
+  let { seed: { value: seed } } = e.target.elements;
+  setupSketch(seed);
+  return false;
+}
+
+const drawRandomSketch = e => {
+  if (e) {
+    e.preventDefault();
+  }
+  const newSeed = Math.round(random(100000000));
+  select('#seed').value(newSeed)
+  setupSketch(newSeed);
+  return false;
+}
+
+const setupSketch = seed => {
+  clear();
+  background(255);
+  randomSeed(seed);
+
+  pixels = [], destinations = []
+
+  for (let i = 60; i < 461; i += 40) {
+    let row = [], destinationsRow = [];
+    for (let j = 60; j < 461; j += 40) {
+      const x = (shouldNegative() * random(30)) + i;
+      const y = (shouldNegative() * random(30)) + j;
+      destinationsRow.push([x, y]);
+      row.push([i, j]);
+    }
+    pixels.push(row);
+    destinations.push(destinationsRow);
+  }
+
+  loop();
+}
+
+const saveCanvasHandler = e => {
+  let filename = e.target.elements.saveFilename.value || 'SkewedGrid';
+  saveCanvas(`${filename}-${Date.now().toString()}`, 'png');
+  e.preventDefault();
+  return false;
+}
+
+const shouldNegative = () => (random(0, 1) > 0.5) ? -1 : 1;
+
+function draw() {
+  background(0);
+  let done = true;
+
+  // move pixels
+  for (let i = 0; i < pixels.length; i++) {
+    for (let j = 0; j < pixels[0].length; j++) {
+      const [destX, destY] = destinations[i][j];
+      const [pixelX, pixelY] = pixels[i][j];
+      if (destX - pixelX > 1 || destY - pixelY > 1) {
+        done = false;
+      }
+      const deltaX = (destX - pixelX) * 0.01;
+      const deltaY = (destY - pixelY) * 0.01;
+      pixels[i][j] = [pixelX + deltaX, pixelY + deltaY];
+    }
+  }
+  
+  // draw
+  for (let i = 1; i < pixels[0].length; i++) {
+    const [currX, currY] = pixels[i][0];
+    const [leftX, leftY] = pixels[i - 1][0];
+    line(currX, currY, leftX, leftY);
+  }
+  
+  for (let i = 1; i < pixels.length; i++) {
+    const [currX, currY] = pixels[0][i];
+    const [aboveX, aboveY] = pixels[0][i - 1];
+    line(currX, currY, aboveX, aboveY);
+  }
+  
+  for (let i = 1; i < pixels.length; i++) {
+    for (let j = 1; j < pixels[0].length; j++) {
+      const [currX, currY] = pixels[i][j];
+      const [aboveX, aboveY] = pixels[i][j - 1];
+      const [leftX, leftY] = pixels[i - 1][j];
+      line(currX, currY, aboveX, aboveY);
+      line(currX, currY, leftX, leftY);
+    }
+  }
+
+  if (done) {
+    noLoop();
+  }
+}
